c13c4de7d4ba57e6ec2f99a9dfde0c23ae2e3c05
Ferit Tunçer <ferit@cryptolab.net>
2021-12-02 13:45:32 +0300

    feat(package): initialized parcel package

---
+++ dist/main.js
@@ -0,0 +1,11 @@
+function $parcel$export(e, n, v, s) {
+  Object.defineProperty(e, n, {get: v, set: s, enumerable: true, configurable: true});
+}
+
+$parcel$export(module.exports, "add", () => $8a4d90d26b78c8dc$export$e16d8520af44a096);
+function $8a4d90d26b78c8dc$export$e16d8520af44a096(a, b) {
+    return a + b;
+}
+
+
+//# sourceMappingURL=main.js.map
